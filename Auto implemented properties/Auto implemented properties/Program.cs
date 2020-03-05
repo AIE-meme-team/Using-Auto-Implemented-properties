f11c5d7571ba4209eb0e48975980599d4087a58e
Zie Mcdowell <59741700+ZieMcd@users.noreply.github.com>
2020-03-05 10:11:19 +0200

    Add files via upload

---
+++ Auto implemented properties/Auto implemented properties/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Auto_implemented_properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rect shape = new Rect { Length = 10.00, Width = 10.00 };
+
+            Console.WriteLine(shape.getArea());
+        }
+    }
+}
---
+++ Auto implemented properties/Auto implemented properties/Rect.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Auto_implemented_properties
+{
+    class Rect
+    {
+        public double Length { get; set; }
+        public double Width { get; set; }
+
+        // Dont need to code a constructor or accessor methods
+
+        public double getArea()
+        {
+            return Length * Width;
+        }
+
+    }
+}
